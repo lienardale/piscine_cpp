9e1f8a435e07401b52afa3a77eab46c88becbef5
alienard <alienard@student.42.fr>
2021-03-03 10:04:46 +0100

    ex00 ok

--- CPP07/ex00/main.cpp
+++ CPP07/ex00/main.cpp
@@ -6,21 +6,31 @@
 /*   By: alienard <alienard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/02 21:10:09 by alienard          #+#    #+#             */
-/*   Updated: 2021/03/02 21:10:34 by alienard         ###   ########.fr       */
+/*   Updated: 2021/03/03 10:02:54 by alienard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main( void ) { int a = 2;
+#include "whatever.hpp"
+
+
+int main( void ) {
+	int a = 2;
 	int b = 3;
+	int e = 3;
 	::swap( a, b );
-	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "a = " << a << ", b = " << b << ", e = " << e << std::endl;
 	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
 	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::cout << "min( a, c ) = " << ::min( a, e ) << std::endl;
+	std::cout << "max( a, c ) = " << ::max( a, e ) << std::endl;
 	std::string c = "chaine1";
 	std::string d = "chaine2";
+	std::string f = "chaine2";
 	::swap(c, d);
-	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "c = " << c << ", d = " << d << ", f = " << f << std::endl;
 	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
 	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	std::cout << "min( c, f ) = " << ::min( c, f ) << std::endl;
+	std::cout << "max( c, f ) = " << ::max( c, f ) << std::endl;
 	return 0;
 }
---
+++ CPP07/ex00/whatever.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alienard <alienard@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/03 09:46:34 by alienard          #+#    #+#             */
+/*   Updated: 2021/03/03 10:02:35 by alienard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#include <iostream>
+#include <string>
+
+template< typename T >
+void	swap( T & x, T & y){
+	T tmp;
+	tmp = x;
+	x = y;
+	y = tmp;
+}
+
+template< typename T >
+T const & min( T const & x, T const & y){
+	return ( x < y ? x : y);
+}
+
+template< typename T >
+T const & max( T const & x, T const & y){
+	return ( x > y ? x : y);
+}
